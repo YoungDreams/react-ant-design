e8496a31fcf6eea6ff716a78dcab751e8713b898
Taylor <258266864@qq.com>
2019-03-10 11:36:02 +0800

    搭建基于 model 的卡片列表页面

--- config/config.js
+++ config/config.js
@@ -1,11 +1,33 @@
 export default {
   plugins: [
     ['umi-plugin-react', {
-      antd: true
+      antd: true,
+      dva: true,
     }],
   ],
   routes: [{
     path: '/',
-    component: './HelloWorld',
-  }]
+    component: '../layout',
+    routes: [
+      {
+        path: '/',
+        component: 'Helloworld',
+      },
+      {
+        path: 'puzzlecards', component: './puzzlecards'
+      },
+      {
+        path: '/helloworld',
+        component: 'Helloworld'
+      },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+        ]
+      },
+    ]
+  }],
 };
